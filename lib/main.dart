512c655b2e276a74f1b2a22c24e713883dba9f74
PhalgunaRao <phalu900@gmail.com>
2020-08-02 14:19:41 +0530

    gradient

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        brightness: Brightness.dark,
+        brightness: Brightness.light,
         primaryColor: Color(0xff009688),
         accentColor: Color(0xff7c4DFF),
         textTheme: TextTheme(
@@ -19,11 +19,33 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Building Layouts'),
+          title: Text('Container App'),
         ),
-        body: Center(
-          child: Text('Hello Geethika')
+        body: Container(
+          margin: EdgeInsets.all(100.0),
+          width: 200.0,
+            height: 200.0,
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            gradient: LinearGradient(
+              begin:Alignment(0.0,-1.0),
+              end:Alignment(0.0,-0.8),
+                tileMode: TileMode.mirror,
+              //tileMode: TileMode.repeated,
+              colors: [Colors.purple[50],Colors.purple[500]]
+            ),
+            shape: BoxShape.rectangle,
+            //All direction
+            borderRadius: BorderRadius.all(
+              Radius.circular(50.0)
+            ),
+            //only
+           /* borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25.0),
+              bottomRight: Radius.circular(25.0)
 
+            )*/
+          ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.lightbulb_outline),
